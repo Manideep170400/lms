6baa232d7876ee971d8642edb918ca595f67ea2b
manideep Reddy <manideepreddy170400@gmail.com>
2024-12-03 15:57:50 +0330

    login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import Card from "@mui/material/Card";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <span>
+        <p>Lagout</p>
+        <LogoutOutlinedIcon />
+      </span>
+      <header className="App-header">
+        <Card variant="outlined" className="card">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="password" />
+          <button> login</button>
+          <button>Create Account</button>
+        </Card>
+      </header>
     </div>
   );
 }
