bd8a6871d2182e1f62e6c84c91684ee96252e21d
manideep Reddy <manideepreddy170400@gmail.com>
2024-12-03 17:04:19 +0330

    fecth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import Card from "@mui/material/Card";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const login = () => {
+    fetch("http://localhost:5000/login", {
+      email: email,
+      password: password,
+    });
+  };
   return (
     <div className="App">
       <span>
@@ -11,9 +21,23 @@ function App() {
       </span>
       <header className="App-header">
         <Card variant="outlined" className="card">
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="password" />
-          <button> login</button>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button onClick={login}> login</button>
           <button>Create Account</button>
         </Card>
       </header>
