7a18fa84d3e855ed81eea4f194991225c06a0468
manideep Reddy <manideepreddy170400@gmail.com>
2024-12-05 17:57:58 +0330

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import SignIn from "./signIn";
 import SignUp from "./signup";
+import Logout from "./logout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/createAccount" element={<SignUp />} />
+          <Route path="/logout" element={<Logout />} />
         </Routes>
       </Router>
     </div>
